9c8758bb51ab44e2fe82f73330c216862eaf00b4
sarra <gusmi.sarra@gmail.com>
2020-12-16 16:47:43 +0100

    first commit

---
+++ client/src/JS/constants/contacts.js
@@ -0,0 +1,11 @@
+export const GET_CONTACTS_LOAD = "GET_CONTACTS_LOAD";
+export const GET_CONTACTS_SUCCESS = "GET_CONTACTS_SUCCESS";
+export const GET_CONTACTS_FAIL = "GET_CONTACTS_FAIL";
+
+export const DELETE_CONTACT = "DELETE_CONTACT";
+// get contact by id
+export const GET_CONTACT = "GET_CONTACT";
+
+// post contact
+export const POST_CONTACT = "POST_CONTACT";
+export const EDIT_CONTACT = "EDIT_CONTACT";
